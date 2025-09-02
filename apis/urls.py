28390450fb45edb329cf225c165b8095e179429c
Favour Olumese <olumesefavor@gmail.com>
2025-09-02 07:59:21 +0100

    Worked on users being able to change their passwords
    > Created templates to handle change of password and notify on the success of the change.

--- apis/urls.py
+++ apis/urls.py
@@ -1,10 +1,24 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('login/', views.user_login, name='login'),
+    path('accounts/password_change/', 
+    auth_views.PasswordChangeView.as_view(
+        template_name='registration/password_change_fm.html',
+        success_url='/accounts/password_change/done/'
+        ), 
+        name='password_change'),
+    
+    path('accounts/password_change/done/', 
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name='registration/password_change_complete.html'
+        ), 
+        name='password_change_done'),
+
     path("accounts/", include("django.contrib.auth.urls")),
     path('enroll/student/', views.enroll_student_fingerprint, name='enroll-student-fingerprint'),
     path('enroll/lecturer/', views.enroll_lecturer_fingerprint, name='enroll-lecturer-fingerprint'),
